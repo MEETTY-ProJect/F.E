838e45e4661be20558f2b9e08ddddc62ce330f97
wlgml9290 <wlgml929056@naver.com>
2025-05-15 20:49:51 +0900

    메인페이지 추가

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from ".//pages/Login";
-import SignUp from ".//pages/SignUp";
-import MainPage from ".//pages/MainPage";
-const App = () => {
-    return (_jsx(Router, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Login, {}) }), _jsx(Route, { path: "/signup", element: _jsx(SignUp, {}) }), _jsx(Route, { path: "/main", element: _jsx(MainPage, {}) })] }) }));
-};
-export default App;
